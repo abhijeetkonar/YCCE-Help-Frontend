c06b013dce5a02b47647b99136d59a3eb8be822f
Abhijeet_Konar <abhijeetkonar01@gmail.com>
2024-10-17 11:46:58 +0530

    routing and initialization added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Papers from './pages/Papers';
+import Profile from './pages/Profile';
+import Upload from './pages/Upload';
+import Practicals from './pages/Practicals';
+import AppLayout from './components/AppLayout';
+import ProtectRoute from './components/ProtectRoute';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const router = createBrowserRouter([
+        {
+            path: "/signin",
+            element: <SignIn />,
+        },
+        {
+            path: "/signup",
+            element: <SignUp />,
+        },
+        {
+            path: "/",
+            element: <AppLayout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "/papers",
+                    element: <Papers />,
+                },
+                {
+                    path: "/practicals",
+                    element: <Practicals />,
+                },
+                {
+                    path: "/profile",
+                    element: <ProtectRoute/>,
+                    children: [
+                        {
+                            path: "/profile",
+                            element: <Profile />,
+                        },
+                    ]
+                },
+                {
+                    path: "/upload",
+                    element: <ProtectRoute/>,
+                    children: [
+                        {
+                            path: "/upload",
+                            element: <Upload />,
+                        },
+                    ]
+                },
+            ]
+        },
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    ]);
+    return <RouterProvider router={router} />;
 }
 
 export default App
